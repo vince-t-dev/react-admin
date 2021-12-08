7a030c1cd378f2e950819c838f2e3eb8449561b9
Blackie <vince@Blackie.local>
2021-12-08 15:11:11 +0900

    structure changes

--- xpr/server_js/simple_echo.js
+++ xpr/server_js/simple_echo.js
@@ -1,14 +1,5 @@
+//this is a "simple echo" JS script to demonstrate binding scripts to elements and datasources.
+
 exports.process = function(context, options) {
-    var api = require("XprObjects/Api");
-    var request = require("XprObjects/WebRequest");
-
-    var users = api.call({
-        method: "GET",
-        uri: "/users/",
-        params: {
-            "per_page": 10
-        }
-    });
-
-    return users;
+    return "Yo "+options["message"];
 }
