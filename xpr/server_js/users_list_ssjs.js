5bbd6a5845012d08ad91ac3c98312cf2dfac5448
Blackie <vince@Blackie.local>
2021-12-10 08:55:16 +0900

    api fetching

--- xpr/server_js/users_list_ssjs.js
+++ xpr/server_js/users_list_ssjs.js
@@ -1,7 +1,7 @@
 //this is a "simple echo" JS script to demonstrate binding scripts to elements and datasources.
 
 exports.process = function(context, options) {
-    var request = require("/XprObjects/WebRequest");
+    var request = require("/xpr/XprObjects/WebRequest");
     let object = context.concat(options);
     return object;
 }
