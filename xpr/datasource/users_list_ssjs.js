42468ee91c2b8a9c2557f7d2312869709ab1173b
Blackie <vince@Blackie.local>
2021-12-09 15:55:33 +0900

    api fetching

--- xpr/datasource/users_list_ssjs.js
+++ xpr/datasource/users_list_ssjs.js
@@ -1,5 +1,6 @@
 //this is a "simple echo" JS script to demonstrate binding scripts to elements and datasources.
 
 exports.process = function(context, options) {
-    return context;
+    let object = context.concat(options);
+    return object;
 }
