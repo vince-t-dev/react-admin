5c0f012be158a086ebc91d0c85a1b5727299555d
Blackie <vince@Blackie.local>
2021-12-10 10:54:59 +0900

    test api

--- xpr/server_js/users_list_ssjs.js
+++ xpr/server_js/users_list_ssjs.js
@@ -2,7 +2,7 @@
 const xpr_objects = require("/xpr/request");
 
 exports.process = function(context, options) {
-    let api = require("Api");
+    let api = xpr_objects.Api();
     let request = xpr_objects.XprRequest();
     
     let users_params = { per_page: 10 };
