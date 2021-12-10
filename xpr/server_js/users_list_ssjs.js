3e8a5f4a9728538892f6cdb3c2a21a75048c9c35
Blackie <vince@Blackie.local>
2021-12-10 10:48:51 +0900

    test api

--- xpr/server_js/users_list_ssjs.js
+++ xpr/server_js/users_list_ssjs.js
@@ -1,9 +1,17 @@
-const request = require("/xpr/request");
-//exports.XprRequest = PHP.XprRequest;
+// this returns all xprobjects
+const xpr_objects = require("/xpr/request");
 
-function process(data) {
+exports.process = function(context, options) {
+    let api = require("/xpr/XprApi");
+    let request = xpr_objects.XprRequest();
     
-    return request.XprRequest();
-}
+    let users_params = { per_page: 10 };
+    if (request.urlParams.q) users_params.q_FirstName_LastName_Email = request.urlParams.q;
+    if (request.urlParams.page) users_params.page = request.urlParams.page;
+    let users = api.call({
+        "uri" : "/users/",
+        "params" : users_params
+    });
 
-exports.process = process;
+    return users;
+}
