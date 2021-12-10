30a0497b275a6efc3d303528bb843327b55422d1
Blackie <vince@Blackie.local>
2021-12-10 09:47:06 +0900

    test api

--- xpr/server_js/users_list_ssjs.js
+++ xpr/server_js/users_list_ssjs.js
@@ -1,9 +1,9 @@
 //this is a "simple echo" JS script to demonstrate binding scripts to elements and datasources.
-var request = XprRequest();
+
 exports.process = function(context, options) {
     //let api = require("/xpr/XprObjects/XprApi");
     //let request = require("/xpr/src/includes/classes/XprObjects/XprRequest");
-
+    var request = require("/xpr/XprRequest");
     //var api = XprApi();
     /*let users_params = { per_page: 10 };
     if (request.urlParams.q) users_params.q_FirstName_LastName_Email = request.urlParams.q;
