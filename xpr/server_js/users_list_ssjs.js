eb116bb392f6a46da3dd41cf1ecdc29922a90278
Blackie <vince@Blackie.local>
2021-12-10 11:11:35 +0900

    test api

--- xpr/server_js/users_list_ssjs.js
+++ xpr/server_js/users_list_ssjs.js
@@ -8,7 +8,7 @@ exports.process = function(context, options) {
     let users_params = { per_page: 10 };
     if (request.urlParams.q) users_params.q_FirstName_LastName_Email = request.urlParams.q;
     if (request.urlParams.page) users_params.page = request.urlParams.page;
-    let users = api.call({
+    let users = api({
         uri : "/users/",
         params : users_params
     });
