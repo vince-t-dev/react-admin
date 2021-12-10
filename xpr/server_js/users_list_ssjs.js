1d48245bc3eab5431a9d205f378d79c34bf28053
Blackie <vince@Blackie.local>
2021-12-10 10:36:07 +0900

    test api

--- xpr/server_js/users_list_ssjs.js
+++ xpr/server_js/users_list_ssjs.js
@@ -1,8 +1,9 @@
 const request = require("/xpr/XprRequest");
+//exports.XprRequest = PHP.XprRequest;
 
 function process(data) {
     
-    return request;
+    return request.XprRequest();
 }
 
 exports.process = process;
